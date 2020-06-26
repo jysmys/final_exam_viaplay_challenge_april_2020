4385ff87d62eadb61694a378cb4a9e2a06398925
japh <jenny@performanceit.se>
2020-06-26 14:20:27 +0200

    update test

--- cypress/integration/fetchMovies.feature.js
+++ cypress/integration/fetchMovies.feature.js
@@ -1,15 +1,16 @@
 describe("Main view is shown", () => {
   beforeEach(() => {
     cy.server();
-    // cy.route({
-    //   method: "GET",
-    //   url: "**/pc-se/serier/samtliga*",
-    //   response: "fixture:movie_list.json",
-    // });
+    cy.route({
+      method: "GET",
+      url: "**/pc-se/serier/samtliga*",
+      response: "fixture:movie_list.json",
+    });
   });
   it("displays movies", () => {
     cy.visit("/");
 
     cy.get(".display-show").should("be.visible");
+    cy.get("img").should("be.visible");
   });
 });
