77f46499af3f97cadfd3097f1f42d7da40990b02
japh <jenny@performanceit.se>
2020-06-26 14:18:48 +0200

    add styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import "./App.css";
 import Movies from "./components/Movies";
+import { Container } from "semantic-ui-react";
 
 const App = () => {
   return (
     <div>
+      <Container
+        style={{ backgroundColor: "black", width: "100%", height: "40px" }}
+      >
+        {" "}
+        <img
+          src="https://kundservice.viaplay.se/wp-content/themes/viaplaycs/assets/dist/images/viaplay_white.svg"
+          style={{ height: "40px" }}
+        />
+      </Container>
       <Movies />
+      <Container
+        style={{ backgroundColor: "black", width: "100%", height: "40px" }}
+      ></Container>
     </div>
   );
 };
